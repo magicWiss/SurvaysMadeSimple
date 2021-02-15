6b24b7baba9c85299dd6583c4c65a15b2b558b6f
magicWiss <mat.wissel@stud.uniroma3.it>
2021-02-15 20:49:50 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express= require('express');    //importo della libreria express
+
+const app=express();        //creazione app express
+                            //app-->utilizzato per la configurazione dell'app
+                            //tutti gli handlers sono inseriti nell' app indicata
+
+
+app.get('/',(req,res)=>{
+  res.send({hi:'there'});
+
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
